0f3aa07c4dc471a14d811fc808c439c069aad44f
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-03-17 01:54:36 +0530

    Update Early_warning.py

--- Early_warning.py
+++ Early_warning.py
@@ -8,10 +8,11 @@ class get_warning:
 		self.charge_rate = BMS_input["charge_rate"]
 		self.check_temperature_warning()
 		self.check_soc_warning()
-		self.check_charge_rate_warning()
+		self.check_charge_rate_warning_low()
+		self.check_charge_rate_warning_high()
 
 	def check_temperature_warning(self):
-		if self.temperature in range(0,5):
+		if self.temperature in range(BMS_allowed_range[temperature][min],int (BMS_allowed_range[temperature][min] + BMS_allowed_range[temperature][max]*0.05+1)):
 			print('Warning: Approaching low_teperature')
 		elif self.temperature in range(40,45):
 			print("Warning: Approaching peak_temperature")
@@ -22,8 +23,10 @@ class get_warning:
 		elif self.soc in range(75,80):
 			print("Warning: Approaching peak_temperature")
 
-	def check_charge_rate_warning(self):
+	def check_charge_rate_warning_low(self):
 		if (self.charge_rate <= 0.1 ):
 			print('Warning: Approaching low_charge_rate')
-		elif (self.charge_rate >= 0.7 ) and (self.charge_rate <= 0.8 ):
+			
+	def check_charge_rate_warning_high(self):
+		if (self.charge_rate >= 0.7 ) and (self.charge_rate <= 0.8 ):
 			print("Warning: Approaching peak_charge_rate")
