98453f85fd8a216be140038a971b4a892a6d9ec5
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-03-17 14:40:48 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     set_language('EN')
     #Early_warnings.get_warning({'temperature' : 3,'soc' : 76,'charge_rate' : 0.7})
     assert(battery_test.check_battery_is_ok({'temperature' : 25,'soc' : 78,'charge_rate' : 0.9}) is False) 
-    assert(battery_test.check_battery_is_ok({'temperature' : 25,'soc' : 77,'charge_rate' : 0.7}) is True)
+    assert(battery_test.check_battery_is_ok({'temperature' : 25,'soc' : 70,'charge_rate' : 0.5}) is True)
     assert(battery_test.check_battery_is_ok({'temperature' : 50,'soc' : 85,'charge_rate' : 0}) is False)
