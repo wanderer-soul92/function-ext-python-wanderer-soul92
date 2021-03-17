fe982624f26acf54a1002a9d3dcc5135f03268ef
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-03-17 09:40:53 +0530

    Add files via upload

--- battery_test.py
+++ battery_test.py
@@ -1,23 +1,33 @@
+import Early_warnings
+
 BMS_allowed_range = {'temperature': {'min': 0, 'max': 45},
                 'soc': {'min': 20, 'max': 80},
                 'charge_rate': {'min': 0, 'max': 0.8}}
 
-
 def get_out_of_range_parameter(BMS_input):
-    out_of_range_parameters = []
+    out_of_range_parameters = {}
+    actions_on_parameters ={}
     for parameter,value in BMS_input.items() :
-        check_tolerance_range(parameter,value,out_of_range_parameters)
+        check_tolerance_range(parameter,value,out_of_range_parameters,actions_on_parameters)
        
-    return out_of_range_parameters
+    return out_of_range_parameters, actions_on_parameters
 
-def check_tolerance_range(key,value,out_of_range_parameters):
-     if (value < BMS_allowed_range[key]['min']) or (value > BMS_allowed_range[key]['max']):
-            out_of_range_parameters.append(key)
+def check_tolerance_range(parameter,value,out_of_range_parameters,actions_on_parameters):
+     if (value < BMS_allowed_range[parameter]['min']):
+            out_of_range_parameters[parameter] = " Low Breach"
+            actions_on_parameters[parameter]= 'value to be increased'
+     elif (value > BMS_allowed_range[parameter]['max']):
+            out_of_range_parameters[parameter] = " High Breach"
+            actions_on_parameters[parameter]= 'value to be decreased'
+     else :
+         Early_warnings.get_warning(parameter,value,out_of_range_parameters,actions_on_parameters)
+
+
 
 
 def check_battery_is_ok(BMS_input):
-    out_of_range_parameter_count = get_out_of_range_parameter(BMS_input)
-    if len(out_of_range_parameter_count) == 0: 
+    out_of_range_parameters, actions_on_parameters = get_out_of_range_parameter(BMS_input)
+    if len(out_of_range_parameters) == 0: 
         return True
     else :
         return False
